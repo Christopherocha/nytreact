c8388d4d360a41b326c83870c9775c091ad42e30
Chris R <chris.rocha@>
2017-09-11 22:18:16 -0500

    basics of main component

---
+++ app/js/components/Main.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {Link} from "react-router";
+
+export default class Main extends React.Component{
+    constructor(props){
+      super(props);
+    }
+    
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate() {
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
